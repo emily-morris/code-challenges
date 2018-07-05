3ab81df92d538f2e76671d6fa15e37ee5eb3c5ee
Emily Morris <morris.emilyanne@gmail.com>
2018-07-05 11:13:04 -0500

    completed challenge

---
+++ find-capitals.js
@@ -0,0 +1,7 @@
+// Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+var capitals = function (word) {
+	//return array with indexes
+	//word = ChuMPs, return [0, 3, 4]
+	//
+};
